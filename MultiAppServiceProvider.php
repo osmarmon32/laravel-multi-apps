30cefa7ac3d8b607cc112acd60bccda822143214
Oscar Mendoza <osmarmon32@gmail.com>
2019-06-08 09:01:42 -0500

    add install instructions and remove comments

--- MultiAppServiceProvider.php
+++ MultiAppServiceProvider.php
@@ -14,13 +14,6 @@ class MultiAppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //$this->app->make('wisdmLabs\todolist\TodolistController');
-		/*
-		
-    $this->mergeConfigFrom(
-        __DIR__.'/path/to/config/courier.php', 'courier'
-    );
-		*/
     }
 
     /**
@@ -35,22 +28,5 @@ class MultiAppServiceProvider extends ServiceProvider
                 CreateNewApp::class
             ]);
         }
-        /*		
-        $this->loadRoutesFrom(__DIR__.'/routes.php');
-        $this->loadMigrationsFrom(__DIR__.'/migrations');
-		$this->loadTranslationsFrom(__DIR__.'/path/to/translations', 'courier');
-        $this->loadViewsFrom(__DIR__.'/views', 'todolist');
-        $this->publishes([
-            __DIR__.'/views' => base_path('resources/views/wisdmlabs/todolist'),
-        ]);
-		
-		if ($this->app->runningInConsole()) {
-			$this->commands([
-				FooCommand::class,
-				BarCommand::class,
-			]);
-		}
-		*/
-		//php artisan vendor:publish --tag=reddireccion\modelsscaffold\ModelsScaffoldServiceProvider  
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # laravel-multi-apps
 Run multple laravel applications using the same instance
+Installation:
+1.- Copy this content to packages/reddireccion/multiapps (create the folder structure if does not exists)
+2.- Add the namespace to the composer file
+	autoload
+		psr-4
+			"Reddireccion\\MultiApps\\":"packages/reddireccion/multiapps"
+3.- Add the service provider Reddireccion\MultiApps\MultiAppServiceProvider::class to your config/app.php
